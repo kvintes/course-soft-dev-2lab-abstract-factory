c4d8f50dbd0453c5cc469c67cae6d6bb8d8320d3
kvintes <vnm111195.ru@gmail.com>
2024-04-23 11:09:21 +0700

    add myclass template

--- MyClass.cpp
+++ MyClass.cpp
@@ -1,3 +1,1 @@
 #include "MyClass.h"
-
-MyClass::MyClass() {}
--- MyClass.h
+++ MyClass.h
@@ -1,10 +1,20 @@
 #ifndef MYCLASS_H
 #define MYCLASS_H
+#include <iostream>
 
-class MyClass
-{
+class MyClass {
 public:
-    MyClass();
+    void testFunc1() {
+    }
+    virtual void testFunc3() const {
+    }
+protected:
+    static void testFunc4() {
+        printf("Hello, world!\n");
+    }
+private:
+    static void testFunc2() {
+    }
 };
 
 #endif // MYCLASS_H
